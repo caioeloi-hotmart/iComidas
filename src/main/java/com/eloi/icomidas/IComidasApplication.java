a003945c6e4faf9b9b7bdc686fb78b17202aa9e4
caioeloi-hotmart <caio.eloi@hotmart.com>
2023-02-02 13:59:44 -0300

    Start project

---
+++ src/main/java/com/eloi/icomidas/IComidasApplication.java
@@ -0,0 +1,13 @@
+package com.eloi.icomidas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IComidasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IComidasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eloi/icomidas/IComidasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eloi.icomidas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IComidasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
