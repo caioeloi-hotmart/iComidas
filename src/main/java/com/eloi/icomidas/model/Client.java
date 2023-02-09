6afc9eb6653b902f9511ed78fa13dad1f9656c39
caioeloi-hotmart <caio.eloi@hotmart.com>
2023-02-09 14:29:14 -0300

    Adding @Component to dependencies

---
+++ src/main/java/com/eloi/icomidas/model/Client.java
@@ -0,0 +1,32 @@
+package com.eloi.icomidas.model;
+
+public class Client {
+    private String name;
+    private String email;
+    private String phone;
+    private boolean active = false;
+
+    public Client(String name, String email, String phone) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public String getPhone() {return phone;}
+
+    public void setPhone(String phone) {this.phone = phone;}
+
+    public boolean isActive() {return active;}
+
+    public void setActive(boolean active) {this.active = active;}
+
+    public String getEmail() {return email;}
+
+    public void setEmail(String email) {this.email = email;}
+
+    public String getName() { return name;}
+
+    public void setName(String name) {this.name = name;}
+
+    public void activate() {this.active = true; }
+}
